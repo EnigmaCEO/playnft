8b6e4f2a701bcb1641f343ad82bdace539aab92f
Xavier Moore <ceo@enigma-games.com>
2022-04-20 17:45:01 -0500

    View All button

--- components/streamers.js
+++ components/streamers.js
@@ -11,7 +11,8 @@ class Streamers extends HTMLElement {
 
             <header class="section-header">
                 <h2>Streamers</h2>
-                <p>Collect Tokens from your favorite Twitch Streamers.</p>
+                <p>Collect Tokens from your favorite Twitch Streamers.</p><br/>
+                <span><button type="submit" onclick="window.location.assign('/streamers/')">View All</button></span>
             </header>
 
             <div class="testimonials-slider swiper-container" data-aos="fade-up" data-aos-delay="200">
